a43563b052b8ce785fbea23179bb732661965c0a
Nahuel <nahuelsaad2002@gmail.com>
2024-04-15 08:30:58 -0300

    actualice el CustomData

--- Ejercicio1/src/data-structure/CustomData.ts
+++ Ejercicio1/src/data-structure/CustomData.ts
@@ -1,3 +1,8 @@
 export class CustomData{
-    data: string = "";
+    nombre: string = "";
+    apellido: string = "";
+    cedula: string = "";
+    telefono: number = 0;
+    departamento: string = "";
+    necesita_asistencia_movilidad: boolean = false;
 }
